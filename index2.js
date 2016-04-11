75025b4a3cb9dc14dc4c26401eb6bc1a298a2df1
Daniel Zehe <daniel.zehe@tum-create.edu.sg>
2016-04-11 23:13:33 +0800

    Added: 	- training test after the iterations 		- basic data cleaning when creating input data sets

--- index2.js
+++ index2.js
@@ -3,10 +3,24 @@ var os = require('os');
 
 	var output_train = [];
 	var output_test = [];
+	var output_all =[];
 fs.readFile('/Users/danielzehe/Dropbox/perfect\ date/klarchiv_03126_daily_his/produkt_klima_Tageswerte_18810101_20151231_03126.txt','utf8',function(err,data){
 	var lines =data.split(os.EOL);
 
-	for(var i=1;i<lines.length;i++){
+
+	var min_val = 100;
+	var max_val = -100;
+	for(var i=25000;i<lines.length;i++){
+		var oneline = lines[i].split(';');
+
+
+		var airtemp = oneline[3];
+
+		min_val=Math.min(airtemp,min_val);
+		max_val=Math.max(airtemp,max_val);
+	}
+	console.log('min:'+min_val+" max:"+max_val);
+	for(var i=25000;i<lines.length;i++){
 		var oneline = lines[i].split(';');
 
 
@@ -15,13 +29,31 @@ fs.readFile('/Users/danielzehe/Dropbox/perfect\ date/klarchiv_03126_daily_his/pr
 		var month = oneline[1].substr(4,2);
 		var day = oneline[1].substr(6,2);
 		// console.log(year);
-		if(i>lines.length/2){
+		var outputobject={};
+		if(i<25005){
+		outputobject ={
+			input:[1,1,1,1, Number(month)/12,Number(day)/31,1],
+			output:[(Number(airtemp)-min_val)/(max_val-min_val)]
+		};
+		}
+		else{
+		outputobject ={
+			input:[output_all[i-25000-4].output[0],output_all[i-25000-3].output[0],output_all[i-25000-2].output[0],output_all[i-25000-2].output[0], Number(month)/12,Number(day)/31,1],
+			output:[(Number(airtemp)-min_val)/(max_val-min_val)]
+		};
+		}
+		 
+
+		output_all.push(outputobject);
+
+
+		if(i%10==0){
 		// if(i==1){
-			output_test.push({input:[Number(year),Number(month),Number(day)],output:[Number(airtemp)]});
+			output_test.push(outputobject);
 		}
 
 		else{
-			output_train.push({input:[Number(year),Number(month),Number(day)],output:[Number(airtemp)]});
+			output_train.push(outputobject);
 		}
 	}
 
--- index3.js
+++ index3.js
@@ -1,21 +1,37 @@
 var syn = require('synaptic');
 var fs = require('fs');
-var perceptron = new syn.Architect.LSTM(3,10,3,1);
+var perceptron = new syn.Architect.LSTM(7,30,1);
 var trainer = new syn.Trainer(perceptron);
 
-fs.readFile('./train50.json','utf8',function(err,data){
+fs.readFile('./test50.json','utf8',function(err,data){
 	var traindata = JSON.parse(data);
 
 	console.log('data loaded... starting training');
 	trainer.train(traindata,{
-		iterations:50,
+		iterations:500,
 		error:.00001,
-		rate:1,
-		log:true
+		rate:0.01,
+		log:true,
+		cost:syn.Trainer.cost.MSE
 	});
 
 
+	console.log('training done');
+
+
+	var outputs =[];
+	for(var i=0;i<300;i++){
+	 outputs.push({
+	 	input: traindata[i].input,
+		output: perceptron.activate(traindata[i].input)[0].toFixed(3),
+		target: traindata[i].output
+	});
+	}
+
+	console.log(outputs);
+
 });
+
 
 
 
